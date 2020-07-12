4a3425df2559dac6a6ba10b892858647433526b7
Nicholas Chedid <nicholas.chedid@mindera.com>
2020-07-12 12:28:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useContext } from "react";
+
+const UserContext = React.createContext(null);
+
+const UserInfo = () => {
+    const user = useContext(UserContext);
+    return <div>{user ? `Welcome ${user.name}` : "Not logged in"}</div>;
+};
+
+const Repositories = () => {
+    const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(
+                "https://api.github.com/users/facebook/repos"
+            );
+            const data = await response.json();
+            setRepositories(data);
+        };
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const filtered = repositories.filter((repo) => repo.favourite);
+        document.title = `You have ${filtered.length} favourites`;
+    }, [repositories]);
+
+    const handleFavourite = (id) => {
+        const newRepositories = repositories.map((repo) => {
+            return repo.id === id
+                ? { ...repo, favourite: !repo.favourite }
+                : repo;
+        });
+        setRepositories(newRepositories);
+    };
+
+    return (
+        <ul>
+            {repositories.map((repo) => (
+                <li key={repo.id}>
+                    {repo.name} {repo.favourite && <span>(Favourite)</span>}
+                    <button onClick={() => handleFavourite(repo.id)}>
+                        Favourite
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [repositories, setRepositories] = useState([]);
+    const [user, setUser] = useState(null);
+
+    /* useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(
+                "https://api.github.com/users/facebook/repos"
+            );
+            const data = await response.json();
+            setRepositories(data);
+        };
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const filtered = repositories.filter((repo) => repo.favourite);
+        document.title = `You have ${filtered.length} favourites`;
+    }, [repositories]);
+
+    const handleFavourite = (id) => {
+        const newRepositories = repositories.map((repo) => {
+            return repo.id === id
+                ? { ...repo, favourite: !repo.favourite }
+                : repo;
+        });
+        setRepositories(newRepositories);
+    }; */
+
+    return (
+        <UserContext.Provider value={user}>
+            <div>
+                <button onClick={(event) => setUser({ name: "John" })}>
+                    Login
+                </button>
+                <UserInfo />
+            </div>
+            {user === null ? " " : <Repositories />}
+            {/* <ul>
+                {repositories.map((repo) => (
+                    <li key={repo.id}>
+                        {repo.name} {repo.favourite && <span>(Favourite)</span>}
+                        <button onClick={() => handleFavourite(repo.id)}>
+                            Favourite
+                        </button>
+                    </li>
+                ))}
+            </ul> */}
+        </UserContext.Provider>
+    );
 }
 
 export default App;
